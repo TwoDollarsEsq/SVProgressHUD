76ee3c02a9c02ee42f4a43bbb375d3c2b5e45f9f
Artyom Rudakov <two_dollars@icloud.com>
2020-01-20 11:36:40 +0200

    Adds umbrella header

---
+++ SVProgressHUD/include/SVProgressHUD.h
@@ -0,0 +1,12 @@
+//
+//  SVIndefiniteAnimatedView.h
+//  SVProgressHUD, https://github.com/SVProgressHUD/SVProgressHUD
+//
+//  Copyright (c) 2014-2019 Guillaume Campagna. All rights reserved.
+//
+
+#import <../SVProgressHUD-Prefix.pch>
+
+#import <../SVIndefiniteAnimatedView.h>
+#import <../SVProgressAnimatedView.h>
+#import <../SVRadialGradientLayer.h>
